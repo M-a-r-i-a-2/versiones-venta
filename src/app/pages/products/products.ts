6fcbb06f6989dc007f8743ffc86df42351a952d9
M-a-r-i-a-2 <mrsb200704@gmail.com>
2025-06-16 13:37:28 -0600

    Creacion de modal y diseño

--- src/app/pages/products/products.ts
+++ src/app/pages/products/products.ts
@@ -1,15 +1,35 @@
 import { Component } from '@angular/core';
 import { Product } from '../../services/products';
 import { Products } from '../../services/products';
+import { CommonModule } from '@angular/common'; // <- necesario para *ngIf y *ngFor
+
 @Component({
   selector: 'app-products',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule], // <- aquí lo agregas
   templateUrl: './products.html',
-  styleUrl: './products.css'
+  styleUrls: ['./products.css']
 })
+
 export class Productos {
-  productos : Product[] = [];
-  constructor(private product:Products){
+  productos: Product[] = [];
+  productoSeleccionado: Product | null = null; // <- Estado del modal
+
+  constructor(private product: Products) {
     this.productos = this.product.getProducts();
   }
+
+  seleccionarProducto(producto: Product) {
+    this.productoSeleccionado = producto;
+  }
+
+  cerrarModal() {
+    this.productoSeleccionado = null;
+  }
+  trackProducto(index: number, producto: Product): number {
+  return producto.id;
 }
+
+}
+
+
